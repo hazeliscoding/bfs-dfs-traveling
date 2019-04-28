9594b6938b0ba949785690a35ba3779f39086c19
Joshua Torres <torresj1997@outlook.com>
2019-04-27 20:09:09 -0500

    Implement a pathfinder with BFS traversal

---
+++ include/PathFinder.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <QTimer>
+#include <QElapsedTimer>
+#include <QQueue>
+
+#include "Node.h"
+
+class PathFinder : public QObject
+{
+	Q_OBJECT
+public:
+	explicit PathFinder(QHash<int, Node*> *listOfIds, int rows, int cols, int tick, QObject *parent = nullptr);
+	void Setup(QHash<int, Node*> *listOfIds, int rows, int cols, int tick);
+	void StartBreadthFirstSearch();
+protected:
+	QList<Node*> *GetNeighborNodes(int id);
+	void Stop(Node *node);
+private:
+	QHash<int, Node*> *m_hash;
+	QQueue<Node*> *m_queue;
+	QTimer *m_bfsTick;
+	QElapsedTimer *m_elapsedTimer;
+	quint64 m_timeElapsed;
+
+	int m_rows;
+	int m_cols;
+	int m_tickInterval;
+private slots:
+	void BreadthFirstSearchStep();
+signals:
+	void DisplayGoal(Node *goal);
+};
