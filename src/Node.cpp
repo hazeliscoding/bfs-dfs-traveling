0a24b4f7596a5719d6340bf1f050bff02fbb43d3
Joshua Torres <torresj1997@outlook.com>
2019-04-27 20:05:43 -0500

    Track previous and visited nodes

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,17 +1,22 @@
 #include "Node.h"
 
 Node::Node(const int id, const int x, const int y, const int size)
-    : m_id(id), m_wall(false), m_start(false), m_goal(false)
+	: m_id(id)
+	, m_previous(nullptr)
+	, m_wall(false)
+	, m_start(false)
+	, m_goal(false)
+	, m_visited(false)
 {
-    // Create visual representation of the node
-    this->m_shape = new QGraphicsRectItem(x, y, size, size);
+	// Create visual representation of the node
+	this->m_shape = new QGraphicsRectItem(x, y, size, size);
 
-    // Set position
-    this->m_position = new QPointF(x, y);
+	// Set position
+	this->m_position = new QPointF(x, y);
 
-    // Description of the node
-    this->m_desc = new QGraphicsTextItem("", this->m_shape);
-    this->m_desc->setPos(this->m_position->x(), this->m_position->y());
+	// Description of the node
+	this->m_desc = new QGraphicsTextItem("", this->m_shape);
+	this->m_desc->setPos(this->m_position->x(), this->m_position->y());
 }
 
 Node::~Node()
@@ -49,6 +54,16 @@ bool Node::IsGoal() const
     return this->m_goal;
 }
 
+bool Node::WasVisited() const
+{
+	return this->m_visited;
+}
+
+Node* Node::GetPreviousNode() const
+{
+	return this->m_previous;
+}
+
 void Node::SetDescription() const
 {
     this->m_desc->setPlainText(QString::number(this->m_id));
@@ -68,6 +83,37 @@ void Node::SetGoal(const bool goal)
         this->m_shape->setBrush(QBrush(Qt::red));
 }
 
+void Node::SetActive(bool active)
+{
+	if (active)
+	{
+		this->m_shape->setBrush(QBrush(Qt::yellow));
+	}
+	else
+	{
+		this->m_shape->setBrush(QBrush(Qt::white));
+	}
+}
+
+void Node::SetPreviousNode(Node *node)
+{
+	this->m_previous = node;
+}
+
+void Node::SetVisited(bool visited)
+{
+	this->m_visited = visited;
+
+	if (visited)
+	{
+		this->m_shape->setBrush(QBrush(Qt::cyan)); // Dark green
+	}
+	else
+	{
+		this->m_shape->setBrush(QBrush(Qt::white));
+	}
+}
+
 void Node::SetWall()
 {
     if (this->m_start || this->m_goal)
@@ -81,5 +127,10 @@ void Node::UnsetWall()
 {
     this->m_wall = false;
     this->m_shape->setBrush(QBrush(Qt::white));
+}
+
+void Node::TracePath()
+{
+	this->m_shape->setBrush(QBrush(Qt::yellow));
 }
 
